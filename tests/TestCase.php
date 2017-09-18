e8b024b61a4d64628e7a4ab7b62159a6fff6fe13
Jaggy Gauran <i.am@jag.gy>
2017-09-18 12:41:25 +0800

    Set up Laravel dusk and speed up testing.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,35 @@
 
 namespace Tests;
 
+use PHPUnit\Framework\Assert;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Hash;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    function setUp()
+    {
+        parent::setUp();
+
+        $this->assertContains();
+
+        $this->dumbDownEncryption();
+    }
+
+    private function assertContains()
+    {
+        EloquentCollection::macro('assertContains', function ($assertion) {
+            return tap(collect($this->items), function ($collection) use ($assertion) {
+                Assert::assertTrue($collection->contains($assertion));
+            });
+        });
+    }
+
+    private function dumbDownEncryption()
+    {
+        Hash::setRounds(4);
+    }
 }
