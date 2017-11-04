3c7475258138361e1e491d2cf7f47ab61a8bc786
Jaggy Gauran <i.am@jag.gy>
2017-11-05 01:35:02 +0800

    Add tailwind css for our utilities.

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,10 +1,10 @@
 <form method="POST" action="{{ route('register.store') }}">
     {{ csrf_field() }}
 
-    <input name="name" type="text" value=""/>
-    <input name="email" type="text" value=""/>
-    <input name="password" type="password" value=""/>
-    <input name="password_confirmation" type="password" value=""/>
+    <input name="name" type="text">
+    <input name="email" type="text">
+    <input name="password" type="password">
+    <input name="password_confirmation" type="password">
 
     <button type="submit">Register</button>
 </form>
