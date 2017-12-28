e7841dcef5a019a36df949f9dacc3621bf13ae5d
Jaggy Gauran <i.am@jag.gy>
2017-12-29 02:07:20 +0800

    Add a base database user seeder.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        $users = [
+        ];
+
+        array_map([User::class, 'create'], $users;
     }
 }
