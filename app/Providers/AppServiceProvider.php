f8d3062959580ee0b60d7a4b4e6567f8e95a3248
Jaggy Gauran <i.am@jag.gy>
2018-02-08 04:58:09 +0800

    Upgrade the project to Laravel 5.6.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         require_once app_path('helpers.php');
+
+        Blade::withoutDoubleEncoding();
 
         view()->composer('*', function ($view) {
             $view->with('me', auth()->user());
