5aacc5b8d090df47bfba1cb931556712f65f9133
Jaggy <jaggy@melody.local>
2018-02-27 13:29:37 +0800

    Only show the links to people who _can_ be impersonated.

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -3,5 +3,7 @@
 @section('content')
     <h1>{{ $user->name }}</h1>
 
+    @canBeImpersonated($user)
     <a href="{{ route('impersonate', $user) }}">Impersonate</a>
+    @endCanBeImpersonated
 @endsection
