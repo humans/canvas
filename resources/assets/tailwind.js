be526932e527efc5855ba5c7498e1279fd2f8ffd
Jaggy Gauran <i.am@jag.gy>
2018-03-16 09:12:57 +0800

    Hard code the text sizes.

--- resources/assets/tailwind.js
+++ resources/assets/tailwind.js
@@ -159,15 +159,13 @@ module.exports = {
     */
 
     textSizes: {
-        'xs':   '.75rem',     // 12px
-        'sm':   '.875rem',    // 14px
-        'base': '1rem',       // 16px
-        'lg':   '1.125rem',   // 18px
-        'xl':   '1.25rem',    // 20px
-        '2xl':  '1.5rem',     // 24px
-        '3xl':  '1.875rem',   // 30px
-        '4xl':  '2.25rem',    // 36px
-        '5xl':  '3rem',       // 48px
+        'xs':   '12px',
+        'sm':   '16px',
+        'base': '24px',
+        'lg':   '34px',
+        'xl':   '48px',
+        '2xl':  '68px',
+        '3xl':  '96px',
     },
 
 
