e81a21ccf606afd7b7a88ee8cf6b972be9cbfbb2
Jaggy Gauran <jaggy@artisan.studio>
2018-06-26 23:24:01 +0800

    Hide the admin link on standard users.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="wrapper">
         <h1>Home</h1>
 
-        <p class="[ text-grey-darker ms-sm ]">
+        <p class="[ text-grey-darker ]">
             Hey <strong v-html="user.name"></strong>! This part is a small demo of Laravel Passport working out of the box.
         </p>
     </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,10 +10,12 @@
                     @impersonating
                         <li><a href="{{ route('impersonate.leave') }}">End Impersonation</a></li>
                     @else
-                        <li><a href="{{ route('admin.dashboard') }}">Admin</a></li>
+                        @canImpersonate
+                            <li><a href="{{ route('admin.dashboard') }}">Admin</a></li>
+                        @endCanImpersonate
                     @endImpersonating
 
-                    <li class="[ ml-16 ]"><a href="{{ route('logout') }}">Sign Out</a></li>
+                    <li class="[ ml-4 ]"><a href="{{ route('logout') }}">Sign Out</a></li>
                 </ul>
             @else
                 <a class="button button-primary" href="{{ route('login') }}">Login</a>
