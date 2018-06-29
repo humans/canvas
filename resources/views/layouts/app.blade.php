0f0fb98869e7d35243dd15041eb0fe8b13f7e624
Jaggy Gauran <jaggy@artisan.studio>
2018-06-30 03:48:30 +0800

    Disable indexing when the app is not in production.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
+        @if(! app()->environment('production'))
+        <meta name="robots" content="noindex">
+        @endif
+
         <title>
            @yield('title', config('app.name'))
         </title>
